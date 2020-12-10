3018d3d5b4caa0777c9e144c0dc20e271101227c
khan Rayees <krayees282@gmail.com>
2020-12-10 23:57:18 +0530

    Github Users Search React App

---
+++ GithubAccountSearchProject/src/context/mockData.js/mockFollowers.js
@@ -0,0 +1,22 @@
+export default [
+  {
+    "login": "Sainik11",
+    "id": 42866144,
+    "node_id": "MDQ6VXNlcjQyODY2MTQ0",
+    "avatar_url": "https://avatars1.githubusercontent.com/u/42866144?v=4",
+    "gravatar_id": "",
+    "url": "https://api.github.com/users/Sainik11",
+    "html_url": "https://github.com/Sainik11",
+    "followers_url": "https://api.github.com/users/Sainik11/followers",
+    "following_url": "https://api.github.com/users/Sainik11/following{/other_user}",
+    "gists_url": "https://api.github.com/users/Sainik11/gists{/gist_id}",
+    "starred_url": "https://api.github.com/users/Sainik11/starred{/owner}{/repo}",
+    "subscriptions_url": "https://api.github.com/users/Sainik11/subscriptions",
+    "organizations_url": "https://api.github.com/users/Sainik11/orgs",
+    "repos_url": "https://api.github.com/users/Sainik11/repos",
+    "events_url": "https://api.github.com/users/Sainik11/events{/privacy}",
+    "received_events_url": "https://api.github.com/users/Sainik11/received_events",
+    "type": "User",
+    "site_admin": false
+  },
+];
